2bb3efb7bc01bb0770c932c9a6d72a47c1bc3c53
Nick Le Large <nick@le-large.com>
2023-08-05 00:23:56 +0200

    feat: /api/users get and post now working

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const app = express()
+const bodyParser = require('body-parser');
 const cors = require('cors')
+const express = require('express')
 require('dotenv').config()
+
+const { User, saveNewUser } = require('./db');
+
+const app = express()
 
 app.use(cors())
 app.use(express.static('public'))
@@ -9,7 +13,27 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
+app.post('/api/users', async (req, res) => {
+  try {
+    let newUser = await saveNewUser(req.body.username);
+    res.json(newUser);
+  } catch (err) {
+    console.log(err);
+  }
+})
+
+app.get('/api/users', async (req, res) => {
+  try {
+    let users = await User.find();
+    console.log(users);
+    res.json(users);
+  } catch (err) {
+    console.log(err);
+  }
+})
 
 
 
