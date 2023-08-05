8940e9f6ce34fb456823d05a8ce473a0b37a43a0
Nick Le Large <nick@le-large.com>
2023-08-05 22:22:25 +0200

    created exerciseSchema

--- db.js
+++ db.js
@@ -7,17 +7,22 @@ const userSchema = new mongoose.Schema({
   username: {type: String, required: true, unique: true}
 });
 
-
 const User = mongoose.model('User', userSchema);
 
 async function saveNewUser(username) {
-    try {
-      const newUser = new User({ username: username });
-      await newUser.save();
-      return newUser;
-    } catch (error) {
-      throw error;
-    }
+  try {
+    const newUser = new User({ username: username });
+    await newUser.save();
+    return newUser;
+  } catch (error) {
+    throw error;
   }
+}
+
+const exerciseSchema = new mongoose.Schema({
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: {type: String, required: true}
+});
 
 module.exports = { User, saveNewUser };
