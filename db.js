bc1d422b4f290fa1b6f308ca6312d3e7243da359
ll-nick <github@le-large.net>
2023-08-05 22:51:27 +0200

    Add exercise functions

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ mongoose.connect(process.env.MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTo
 console.log("Connected to database");
 
 const userSchema = new mongoose.Schema({
-  username: {type: String, required: true, unique: true}
+  username: { type: String, required: true, unique: true }
 });
 
 const User = mongoose.model('User', userSchema);
@@ -20,9 +20,30 @@ async function saveNewUser(username) {
 }
 
 const exerciseSchema = new mongoose.Schema({
-  description: {type: String, required: true},
-  duration: {type: Number, required: true},
-  date: {type: String, required: true}
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: { type: String, required: true }
 });
 
-module.exports = { User, saveNewUser };
+const Exercise = mongoose.model('Exercise', exerciseSchema);
+
+async function saveNewExercise(description, duration, dateStr = null) {
+  try {
+    let date = new Date();
+    if (dateStr) {
+      date = new Date(dateStr)
+    }
+    const newExercise = new Exercise({
+      description: description,
+      duration: duration,
+      date: date.toDateString()
+    });
+    await newExercise.save();
+    return newExercise;
+  } catch (error) {
+    throw error;
+  }
+}
+
+
+module.exports = { User, Exercise, saveNewUser, saveNewExercise };
